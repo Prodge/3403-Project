b8068aebd822bae08cd4b8fe992a40f5e7eaa45a
Prodge <prodge@prodge.net>
2016-04-21 20:23:44 +0800

    Started progress - got includes working with jquery

---
+++ root/scripts/includes.js
@@ -0,0 +1,7 @@
+var page_elements_dir = "page_elements/";
+
+// Loading the constant page elements on the given divs
+$("#nav").load(page_elements_dir + "nav.html");
+$("#footer").load(page_elements_dir + "footer.html");
+
+console.log($('#nav'));
