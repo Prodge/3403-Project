8af730e6022b88567dcfafe307970f952c88ad1e
Prodge <prodge@prodge.net>
2016-04-22 00:17:29 +0800

    got the box to fall off the screen

--- root/scripts/game.js
+++ root/scripts/game.js
@@ -5,44 +5,78 @@ var ctx = canvas.getContext('2d');
 var width = canvas.width;
 var height = canvas.height;
 
-ctx.beginPath();
-ctx.moveTo(25,25);
-ctx.lineTo(105,25);
-ctx.lineTo(25,105);
-ctx.fill();
 
-function update(){
-    friction = 0.2;
-    gravity = 0.8;
-
-}
-
-player = {
+var player = {
     x: width/2,
     y: height/2,
     vel_x: 0,
     vel_y: 0,
-    jumping: false,
+    //jumping: false,
 }
+console.log(width, height)
 
-if (keys[38] || keys[32]) {
-    // up arrow or space
-    if(!player.jumping){
-        player.jumping = true;
-        player.vel_y = -player.speed*2;
-    }
-}
+var friction = 0.2;
+var gravity = 0.8;
+var keys = [];
 
-if (keys[39]) {
-    // right arrow
-    if (player.vel_x < player.speed) {
-        player.vel_x++;
-    }
-}
+// Key listeners
+document.body.addEventListener("keydown", function(e) {
+    console.log("pressed: ", e.keycode);
+    keys[e.keyCode] = true;
+});
 
-if (keys[37]) {
-    // left arrow
-    if (player.vel_x > -player.speed) {
-        player.vel_x--;
+document.body.addEventListener("keyup", function(e) {
+    keys[e.keyCode] = false;
+});
+
+function render(){
+
+    if (keys[38] || keys[32]) {
+        // up arrow or space
+        if(!player.jumping){
+            //player.jumping = true;
+            player.vel_y = -player.speed*2;
+        }
     }
+
+    if (keys[39]) {
+        // right arrow
+        if (player.vel_x < player.speed) {
+            player.vel_x++;
+        }
+    }
+
+    if (keys[37]) {
+        // left arrow
+        if (player.vel_x > -player.speed) {
+            player.vel_x--;
+        }
+    }
+
+    player.vel_x *= friction;
+
+    player.vel_y += gravity;
+
+    player.x += player.vel_x;
+    player.y += player.vel_y;
+
+    if (player.x >= width) {
+        player.x = width-player.width;
+    } else if (player.x <= 0) {
+        player.x = 0;
+    }
+
+    if(player.y >= height){
+        player.y = height;
+        //player.jumping = false;
+    }
+
+    ctx.clearRect(0,0,width,height);
+    ctx.fillStyle = "red";
+    ctx.fillRect(player.x, player.y, 20, 20);
+    console.log('working', player)
+
+    requestAnimationFrame(render);
+
 }
+render();
