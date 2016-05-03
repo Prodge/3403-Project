e8af3c23aea948c60ed77e8e9acc4021eb81df01
Prodge <prodge@prodge.net>
2016-05-04 04:58:41 +0800

    Wait for document ready before starting game script (wait for jquery to load)

--- root/static/js/game.js
+++ root/static/js/game.js
@@ -6,20 +6,21 @@
  *
  */
 
+$(document).ready(function () {
+    var canvas = document.getElementById('game_canvas');
+    var ctx = canvas.getContext('2d');
+    var width = canvas.width;
+    var height = canvas.height;
 
-var canvas = document.getElementById('game_canvas');
-var ctx = canvas.getContext('2d');
-var width = canvas.width;
-var height = canvas.height;
+    var player_standing = document.getElementById("player_standing");
+    var player_jumping = document.getElementById("player_jumping");
+    var player_running_left = document.getElementById("player_running_left");
+    var player_running_right = document.getElementById("player_running_right");
+    var high_score = 0;
 
-var player_standing = document.getElementById("player_standing");
-var player_jumping = document.getElementById("player_jumping");
-var player_running_left = document.getElementById("player_running_left");
-var player_running_right = document.getElementById("player_running_right");
-var high_score = 0;
-
-initialise();
-draw_initial_screen();
+    initialise();
+    draw_initial_screen();
+})
 
 // Key listeners
 $(document).keydown(function(e){
@@ -209,7 +210,7 @@ function run_game(){
 
     update_points();
     render_points();
-    
+
     requestAnimationFrame(run_game);
 }
 
