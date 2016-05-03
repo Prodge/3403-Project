e35247d06f831ea7af1b6a428876fa70a2cbcc6d
Prodge <prodge@prodge.net>
2016-05-04 03:32:57 +0800

    wip

--- root/routes/game.js
+++ root/routes/game.js
@@ -1,6 +1,5 @@
 
-exports.index = function(req, res){
-  //res.render('index', { title: 'Express' });
+exports.game = function(req, res){
   res.render('game', {
     title : "Play Action Box",
   })
