c4556a2d21866db9c02735d191d8afb423e861d4
Prodge <prodge@prodge.net>
2016-05-05 04:56:57 +0800

    Updated server to include modules required for user auth

--- root/server.js
+++ root/server.js
@@ -1,20 +1,22 @@
-
-/**
- * Module dependencies.
- */
 console.log('Starting Server Init')
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express     = require('express')
+var routes      = require('./routes')
+var user        = require('./routes/user')
+var http        = require('http')
+var path        = require('path');
+var app         = express();
+var bodyParser  = require('body-parser');
+var mongoose    = require('mongoose');
+var morgan      = require('morgan');
+var passport	= require('passport');
+var config      = require('./config/database'); // get db config file
+var User        = require('./app/models/user'); // get the mongoose model
+var port        = process.env.PORT || 8080;
+var jwt         = require('jwt-simple');
 
-var app = express();
 
-
-var Mongoose = require('mongoose');
-var db = Mongoose.createConnection('localhost', 'mytestapp');
+//var db = Mongoose.createConnection('localhost', 'mytestapp');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -26,13 +28,17 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'static')));
+app.use(passport.initialize());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-
+// URLs
 app.get('/', routes.game);
 app.get('/instructions', routes.instructions);
 app.get('/theme', routes.theme);
@@ -43,3 +49,4 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
