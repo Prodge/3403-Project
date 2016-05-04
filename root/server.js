80e568d55a2259f99a3cbf8e32bf509576de6796
Prodge <prodge@prodge.net>
2016-05-05 06:06:36 +0800

    Added Test protected route

--- root/server.js
+++ root/server.js
@@ -89,6 +89,38 @@ app.post('/api/authenticate', function(req, res) {
     });
 });
 
+app.get('/api/memberinfo', passport.authenticate('jwt', { session: false}), function(req, res) {
+    var token = getToken(req.headers);
+    if (token) {
+        var decoded = jwt.decode(token, config.secret);
+        User.findOne({
+            name: decoded.name
+        }, function(err, user) {
+            if (err) throw err;
+            if (!user) {
+              return res.status(403).send({success: false, msg: 'Authentication failed. User not found.'});
+            } else {
+              res.json({success: true, msg: 'Welcome in the member area ' + user.name + '!'});
+            }
+        });
+    } else {
+        return res.status(403).send({success: false, msg: 'No token provided.'});
+    }
+});
+
+getToken = function (headers) {
+    if (headers && headers.authorization) {
+        var parted = headers.authorization.split(' ');
+        if (parted.length === 2) {
+            return parted[1];
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
+};
+
 
 // connect the api routes under /api/*
 //app.use('/api', apiRoutes);
