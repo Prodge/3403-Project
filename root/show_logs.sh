7197e34abb861f281b500d1421a761a6b7866ec6
prodge <prodge@prodge.net>
2016-05-10 20:39:24 +0800

    Added script for checking if any console.logs are left behind before committing / merging. Also added this to the post-build process for travis

---
+++ root/show_logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Checks if there are any debug outputs present in the codebase
+
+logs=$(grep -rn "console.log(" | grep -v server | grep -v coverage | grep -v Binary)
+if [[ -z "${logs}" ]]; then
+    echo " ==================================="
+    echo "| Found no debug output statements. |"
+    echo "|       We are good to go!          |"
+    echo " ==================================="
+else
+    echo " ===================================="
+    echo "| Found debug output statements.     |"
+    echo "| Please remove these before merging |"
+    echo " ===================================="
+    echo $logs
+    echo
+    echo
+fi
