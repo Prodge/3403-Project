bf2990342d92d435254d9e47bf0f72039f722190
prodge <prodge@prodge.net>
2016-05-10 16:52:45 +0800

    Refactored start file

--- root/start.js
+++ root/start.js
@@ -1,6 +1,5 @@
-var server = require('./app/server').server;
-var app = require('./app/server').app;
+var server = require('./app/server');
 
-server.listen(app.get('port'), function(){
+server.server.listen(server.app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
