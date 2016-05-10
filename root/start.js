8aadae39862ae1b2b5913101ea9c3484a43bf0d5
prodge <prodge@prodge.net>
2016-05-10 16:44:44 +0800

    Changed server to export a server and an app. Added start file that runs the server. This is so that the server object can be used differently in tests

---
+++ root/start.js
@@ -0,0 +1,6 @@
+var server = require('app/server').server;
+var app = require('app/server').app;
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
--- root/test/routes.js
+++ root/test/routes.js
@@ -1,5 +1,7 @@
 var assert = require('chai').assert;
-var routes = require('../routes/index')
+var routes = require('../routes/index');
+var server = require('../app/server').server;
+var request = require('request')
 
 describe('Array', function() {
   describe('#indexOf()', function () {
@@ -10,8 +12,22 @@ describe('Array', function() {
   });
 });
 
-describe('Instructions', function(){
+describe('server response', function(){
+  before(function (){
+    server.listen(8000);
+    console.log(server)
+  })
   it('should return the title instructions', function(){
-    console.log(routes.instructions(undefined, undefined))
+    console.log('about to test the request')
+    request(server).get('http://127.0.0.1:8000/instructions', function (err, res, body){
+      console.log('test the request')
+      console.log(err)
+      console.log(res)
+      console.log(body)
+      //console.log(routes.instructions(undefined, undefined))
+    });
   });
+  after(function (){
+    server.close();
+  })
 });
