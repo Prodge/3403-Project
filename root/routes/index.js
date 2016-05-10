02518cd4d325db42b093631e5ab7c68bffccd697
prodge <prodge@prodge.net>
2016-05-10 15:51:37 +0800

    retabbed index routes

--- root/routes/index.js
+++ root/routes/index.js
@@ -2,29 +2,29 @@ const fs = require('fs');
 const path = require('path');
 
 exports.instructions = function(req, res){
-    res.render('instructions', {
-        title : "Instructions",
-    })
+  res.render('instructions', {
+    title : "Instructions",
+  })
 };
 
 exports.theme = function(req, res){
-    res.render('theme', {
-        title : "Theme",
-    })
+  res.render('theme', {
+    title : "Theme",
+  })
 };
 
 exports.game = function(req, res){
-    var images = fs.readdirSync(path.join(__dirname, '../static/images/game')).map(function(image){
-        return image.substring(0, image.length - 4);
-    })
-    res.render('game', {
-        title : "Play Action Box",
-        images : images,
-    })
+  var images = fs.readdirSync(path.join(__dirname, '../static/images/game')).map(function(image){
+    return image.substring(0, image.length - 4);
+  })
+  res.render('game', {
+    title : "Play Action Box",
+    images : images,
+  })
 };
 
 exports.author = function(req, res){
-    res.render('author', {
-        title : "Authors",
-    })
+  res.render('author', {
+    title : "Authors",
+  })
 };
